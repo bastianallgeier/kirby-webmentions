3f2fa550d29f3f07cb528f4c941accf1e14cf8e4
Bastian Allgeier <mail@bastianallgeier.com>
2015-05-10 15:50:44 +0200

    added a new like tag

--- helpers.php
+++ helpers.php
@@ -8,3 +8,28 @@ function webmentions($options = array()) {
   return $webmentions;
 
 }
+
+// like tag
+kirbytext::$tags['like'] = array(
+
+  'attr' => array(
+    'text',
+    'title',
+    'rel',
+    'target',
+    'popup'
+  ),
+  'html' => function($tag) {
+
+    return kirbytag(array(
+      'link'   => $tag->attr('like'),
+      'text'   => $tag->attr('text'),
+      'title'  => $tag->attr('title'),
+      'rel'    => $tag->attr('rel'),
+      'target' => $tag->attr('target'),
+      'popup'  => $tag->attr('popup'),
+      'class'  => 'u-like-of',
+    ));
+
+  }
+);
